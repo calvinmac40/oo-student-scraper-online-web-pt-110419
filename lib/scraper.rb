47d18bc5ad18f9af7e52e30be6bec440f2c15015
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-21 12:40:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,46 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-
+  
   def self.scrape_index_page(index_url)
-    
+   web_page = Nokogiri::HTML(open(index_url))
+    list = []
+    web_page.css("div.student-card").each do |student|
+      students =
+      {
+        :name => student.css(".student-name").text,
+        :location => student.css(".student-location").text,
+        :profile_url => student.css("a").attribute('href').value
+      }
+      #binding.pry
+      list << students
+      #binding.pry
+    end
+    list
+    #binding.pry
   end
-
+  
+  
   def self.scrape_profile_page(profile_url)
-    
+    student_profile = {}
+    html = open(profile_url)
+    profile = Nokogiri::HTML(html)
+
+    profile.css("div.main-wrapper.profile .social-icon-container a").each do |social|
+      if social.attribute("href").value.include?("twitter")
+        student_profile[:twitter] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("linkedin")
+        student_profile[:linkedin] = social.attribute("href").value
+      elsif social.attribute("href").value.include?("github")
+        student_profile[:github] = social.attribute("href").value
+      else
+        student_profile[:blog] = social.attribute("href").value
+      end
+    end
+
+    student_profile[:profile_quote] = profile.css("div.main-wrapper.profile .vitals-text-container .profile-quote").text
+    student_profile[:bio] = profile.css("div.main-wrapper.profile .description-holder p").text
+
+    student_profile
   end
-
 end
-
